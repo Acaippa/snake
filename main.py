940157ed34d33337b5f793a1be5513bc7aaecb22
Nils Oliver <106773288+Acaippa@users.noreply.github.com>
2022-08-30 13:44:33 +0200

    sync

--- main.py
+++ main.py
@@ -42,4 +42,47 @@ head = sprites.create(img("""
             1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
         """), SpriteKind.player)
 
-global directionX, directionY
+body_list = []
+
+body_list.append(body)
+
+directionX, directionY = 1, 0
+timer = 0
+
+def move_head():
+    global directionX, directionY, timer
+    if game.runtime() - timer > 1000:
+        head.x += 16 * directionX
+        head.y += 16 * directionY
+        timer = game.runtime()
+    else:
+        timer += 1
+
+def input():
+    global directionX, directionY
+
+    if controller.dx(100) > 0:
+        directionX = 1
+        directionY = 0
+
+    if controller.dx(100) < 0:
+        directionX = -1
+        directionY = 0
+
+    if controller.dy(100) > 0:
+        directionY = 1
+        directionX = 0
+
+    if controller.dy(100) < 0:
+        directionY = -1
+        directionX = 0
+
+def move_body():
+    global body_list
+    print(body_list)
+
+def update():
+    input()
+    move_head()
+
+game.on_update(update)
--- main.ts
+++ main.ts
@@ -39,4 +39,52 @@ let head = sprites.create(img`
             1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
             1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
         `, SpriteKind.Player)
+let body_list = []
+body_list.push(body)
+let [directionX, directionY] = [1, 0]
+let timer = 0
+function move_head() {
+    
+    if (game.runtime() - timer > 1000) {
+        head.x += 16 * directionX
+        head.y += 16 * directionY
+        timer = game.runtime()
+    } else {
+        timer += 1
+    }
+    
+}
 
+function input() {
+    
+    if (controller.dx(100) > 0) {
+        directionX = 1
+        directionY = 0
+    }
+    
+    if (controller.dx(100) < 0) {
+        directionX = -1
+        directionY = 0
+    }
+    
+    if (controller.dy(100) > 0) {
+        directionY = 1
+        directionX = 0
+    }
+    
+    if (controller.dy(100) < 0) {
+        directionY = -1
+        directionX = 0
+    }
+    
+}
+
+function move_body() {
+    
+    console.log(body_list)
+}
+
+game.onUpdate(function update() {
+    input()
+    move_head()
+})
